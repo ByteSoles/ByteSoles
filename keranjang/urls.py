9b2f317bea44042c3b53b625648f375f81f292c2
Farhan Adelio Prayata <farhanadelio10@gmail.com>
2024-10-26 17:57:20 +0700

    fixed

--- keranjang/urls.py
+++ keranjang/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
+from django.urls import path
+from django.contrib.auth import views as auth_views
 app_name = 'keranjang'
 
 urlpatterns = [
@@ -8,9 +9,12 @@ urlpatterns = [
     path('add-to-cart/<int:item_id>/', views.add_to_cart, name='add_to_cart'),
     path('remove-from-cart/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
     path('checkout/', views.checkout, name='checkout_page'),
-    path('payment-successful/', views.payment_successful, name='payment_successful'),
+    # path('payment-successful/', views.payment_successful, name='payment_successful'),
     path('update-quantity/<int:item_id>/', views.update_quantity, name='update_quantity'),
     path('update-quantity-ajax/', views.update_quantity_ajax, name='update_quantity_ajax'),
-    path('remove-from-cart-ajax/', views.remove_from_cart_ajax, name='remove_from_cart_ajax'),]
+    path('remove-from-cart-ajax/', views.remove_from_cart_ajax, name='remove_from_cart_ajax'),
+    path('homepahe/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+
+    ]
 
 
